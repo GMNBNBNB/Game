bbc9452e1014eab9abcc793e247409ff2690b0db
GM <gao.1930@osu.edu>
2024-05-06 00:45:17 -0400

    add

---
+++ Assets/PlayerController2D.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController2D : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
