4698df17e2675f20312f8f4a87ce786215cf7464
GM <gao.1930@osu.edu>
2024-05-07 23:53:38 -0400

    添加物品

---
+++ Assets/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Item")]
+public class Item : ScriptableObject
+{
+    public string Name;
+    public bool stackable;
+    public Sprite icon;
+}
--- Assets/TreeCut.cs
+++ Assets/TreeCut.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class TreeCut : ToolHit
 {
+    [SerializeField] GameObject pickUpDrop;
+    [SerializeField] int dropCount = 5;
+    [SerializeField] float spread = 0.7f;
     public override void Hit()
     {
+        while (dropCount > 0) { 
+            dropCount--;
+
+            Vector3 position = transform.position;
+            position.x += spread * UnityEngine.Random.value - spread / 2;
+            position.y += spread * UnityEngine.Random.value - spread / 2;
+            GameObject go = Instantiate(pickUpDrop);
+            go.transform.position = position;
+        }
         Destroy(gameObject);
     }
 }
