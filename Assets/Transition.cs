bf6cfdc67beabe8586c11bfcbd982ac28d189194
GM <gao.1930@osu.edu>
2024-05-27 22:35:35 -0400

    又g了

---
+++ Assets/Transition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    Transform destination;
+
+    void Start()
+    {
+        destination = transform.GetChild(1);
+    }
+
+    internal void InitiateTransition(Transform toTransition)
+    {
+        toTransition.position = new Vector3(destination.position.x, destination.position.y, toTransition.position.z);
+    }
+
+}
